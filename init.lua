21bad366ab92f3fcccdf9caca36b64d031289c2e
lyes.derradji <lyes.derradji@emeria.eu>
2025-09-28 00:57:46 +0200

    chore/delete-wsl-specific-conf

--- init.lua
+++ init.lua
@@ -1,18 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- Check if running inside WSL
-if vim.fn.has("wsl") == 1 then
-  -- Set up clipboard integration
-  vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-end
