22fd14b67d4a5e426ae95829ccf93abc4a3500d5
lyes.derradji <lyes.derradji@emeria.eu>
2026-02-22 14:41:25 +0100

    feat: add custom biome config to not clash with es-lint

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -26,15 +26,15 @@ local function biome_lsp_or_prettier(bufnr)
 end
 
 return {
-  -- Use Biome instead of prettier / eslint
+  -- Use Biome for most files, ESLint for JSX/TSX
   {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
         ["javascript"] = biome_lsp_or_prettier,
-        ["javascriptreact"] = biome_lsp_or_prettier,
+        ["javascriptreact"] = { "eslint_d" },
         ["typescript"] = biome_lsp_or_prettier,
-        ["typescriptreact"] = biome_lsp_or_prettier,
+        ["typescriptreact"] = { "eslint_d" },
         ["vue"] = { "biome" },
         ["css"] = { "biome" },
         ["scss"] = { "biome" },
@@ -52,10 +52,31 @@ return {
   },
 
   {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
+  {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "biome",
+        "eslint_d",
       },
     },
   },
