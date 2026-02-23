ddc464b7498c46e1a6e3345fbc2534f2957c79b3
lyes.derradji <lyes.derradji@emeria.eu>
2026-02-23 10:30:43 +0100

    feat: add spelling check on markdown

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Enable spell checking for markdown files inside the vault
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = vim.fn.expand("~/SecondBrain") .. "/**/*.md",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "fr,en"
+  end,
+})
